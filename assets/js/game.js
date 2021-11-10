f7473c9d7977597321ebb5505de7f23d3b46c29d
Sodaman <mattrick2214@gmail.com>
2021-11-10 12:51:51 -0600

    added lines of javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+
+    var playerName = window.prompt("your name");
+    console.log(playerName);
+    var robotName = window.prompt("What is your robot's name?");
+    console.log(robotName);
+    console.log("This is how much health you have!");
+// this will do math and log 20
+    console.log(10 + 10);
+// what is this?
+    console.log("Your robot's name is " + robotName);
+// "Your robot, Tony the Robot, has won!
+    console.log("Your robot, " + robotName + ", has won!");
+// fight();
